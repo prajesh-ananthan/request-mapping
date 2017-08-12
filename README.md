beed3b57c9ce78a3bc800723e536d8307e8b1ff6
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2017-08-12 19:28:42 +0800

    + Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # request-mapping
 A simple app demonstrating the use of Request Mapping
+
+## Steps 
+1. Run the application 
+2. Execute run.sh with cURL commands to perform http requests to see resulted String values
---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+URL=http://localhost:8080/home
+
+echo "curl $URL/method0";
+curl $URL/method0;
+printf "\n\n*****\n\n";
+
+echo "curl $URL/method1";
+curl $URL/method1;
+printf "\n\n*****\n\n";
+
+echo "curl -X POST $URL/method2";
+curl -X POST $URL/method2;
+printf "\n\n*****\n\n";
+
+echo "curl -X GET $URL/method2";
+curl -X GET $URL/method2;
+printf "\n\n*****\n\n";
+
+echo "curl -H "name:prajesh" $URL/method3";
+curl -H name:prajesh $URL/method3;
+printf "\n\n*****\n\n";
+
+echo "curl $URL/method4/20/divya";
+curl $URL/method4/20/divya;
+printf "\n\n*****\n\n";
+
+echo "curl $URL/method5/id=10";
+curl $URL/method5?id=10;
+printf "\n\n*****\n\n";
